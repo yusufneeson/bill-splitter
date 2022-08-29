9a7ed2f7a1c83b89688e55010270203503a2b4b8
yusufneeson <yusufneeson@gmail.com>
2022-08-29 14:25:06 +0700

    Calculation: Flexbox

--- src/components/Calculation/Calculation.jsx
+++ src/components/Calculation/Calculation.jsx
@@ -5,8 +5,10 @@ import style from "./calculation.module.css";
 function Calculation() {
 	return (
 		<div className={style.calc}>
-			<TextAmount text="Tip Amount" amount="0.00" />
-			<TextAmount text="Total" amount="0.00" />
+			<div className={style.amounter}>
+				<TextAmount text="Tip Amount" amount="0.00" />
+				<TextAmount text="Total" amount="0.00" />
+			</div>
 			<Button state="disabled">RESET</Button>
 		</div>
 	);

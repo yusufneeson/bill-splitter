02a97b0f93655d8e36f06969ccb10f5b14e05b70
yusufneeson <yusufneeson@gmail.com>
2022-08-29 15:06:28 +0700

    HomeLayout: responsive

--- src/layouts/home.jsx
+++ src/layouts/home.jsx
@@ -1,14 +1,17 @@
 import Logo from "../components/Svgs/Logo";
 import style from "./home.module.css";
+import Link from "next/link";
 
 function HomeLayout({ children }) {
 	return (
 		<div className={style.layout}>
-			<nav className={style.logo}>
-				<a href="/">
-					<Logo />
-				</a>
-			</nav>
+			<header className={style.logo}>
+				<Link href="/">
+					<a href="/">
+						<Logo />
+					</a>
+				</Link>
+			</header>
 			<section className={style.home}>{children}</section>
 		</div>
 	);

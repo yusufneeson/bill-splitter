8b5a5dbdf6639d26ac4fc160485699669fc2dfc5
yusufneeson <yusufneeson@gmail.com>
2022-08-29 20:13:35 +0700

    Calculation: Apply context state

--- src/components/Calculation/Calculation.jsx
+++ src/components/Calculation/Calculation.jsx
@@ -1,13 +1,16 @@
+import { useCalcContext } from "../../context/calc.context";
 import Button from "../Button/Button";
 import TextAmount from "../TextAmount/TextAmount";
 import style from "./calculation.module.css";
 
 function Calculation() {
+	const { state } = useCalcContext();
+
 	return (
 		<div className={style.calc}>
 			<div className={style.amounter}>
-				<TextAmount text="Tip Amount" amount="0.00" />
-				<TextAmount text="Total" amount="0.00" />
+				<TextAmount text="Tip Amount" amount={state.tipamount} />
+				<TextAmount text="Total" amount={state.total} />
 			</div>
 			<Button state="disabled">RESET</Button>
 		</div>

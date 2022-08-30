3f6c297c251155efee2c97dd996a93c24bdbb4ad
yusufneeson <yusufneeson@gmail.com>
2022-08-30 08:48:37 +0700

    Tip: Added function to button tip

--- src/components/Tip/Tip.jsx
+++ src/components/Tip/Tip.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useCalcContext } from "../../context/calc.context";
 import Button from "../Button/Button";
 import TextField from "../TextField/TextField";
@@ -6,11 +6,26 @@ import style from "./tip.module.css";
 
 function Tip() {
 	const { state, tipChange } = useCalcContext();
+	const [btnTip, setBtnTip] = useState(null);
 	const [customTip, setCustomTip] = useState("");
 	const provideTips = [5, 10, 15, 25, 50];
 
+	useEffect(() => {
+		if (Number(state.tip) < 1) {
+			setBtnTip(null);
+			setCustomTip("");
+		}
+	}, [state.tip]);
+
 	const customTipChange = (e) => {
 		setCustomTip(e.target.value);
+		setBtnTip(null);
+		tipChange(e);
+	};
+
+	const handleBtnClick = (e) => {
+		setBtnTip(e.target.value);
+		setCustomTip("");
 		tipChange(e);
 	};
 
@@ -19,7 +34,14 @@ function Tip() {
 			<span className={style.text}>Select Tip %</span>
 			<div className={style.buttons}>
 				{provideTips.map((t) => (
-					<Button key={t}>{t}%</Button>
+					<Button
+						key={t}
+						value={t}
+						onClick={handleBtnClick}
+						state={btnTip == t ? "active" : "normal"}
+					>
+						{t}%
+					</Button>
 				))}
 				<TextField
 					type="number"
